4f3cd96c4375747d3da30e04a9594bcaf6d3b924
Aleksi Vesanto <avjves@gmail.com>
2020-08-04 02:56:28 +0000

    Fixed tesseract coordinates

--- ocr/tesseract.py
+++ ocr/tesseract.py
@@ -13,19 +13,31 @@ class TesseractEngine:
         """
         Given a path to an image, runs said image through tesseract OCR and returns the results.
         """
-        os.system("tesseract {} out txt makebox".format(image_path))
-        text = open("out.txt").read()
-        words = [word for word in text.split()]
-        boxes = open("out.box").readlines()
-        boxes = [line.strip() for line in boxes if not line.startswith("~")]
+        # os.system("tesseract {} out txt makebox".format(image_path))
+        os.system("C:/Tesseract-OCR/tesseract {} out tsv".format(image_path))
+        tsv = open("out.tsv", errors="ignore").readlines()
         results = []
-        for word in words:
-            chars = boxes[:len(word)]
-            values = [v.split(" ") for v in chars]
-            boxes = boxes[len(word):]
-            left = min([float(v[1]) for v in values])
-            bottom = min([float(v[2]) for v in values])
-            right = max([float(v[3]) for v in values])
-            top = max([float(v[4]) for v in values])
-            results.append([word, [left, bottom, right, top]])
+        for line in tsv[1:]:
+            splits = line.split()
+            if not len(splits) == 12:
+                continue
+            else:
+                left, top, width, height = [float(val) for val in splits[6:10]]
+                text = splits[11]
+                results.append([text, [left, top, width, height]])
+        # text = open("out.txt").read()
+        # words = [word for word in text.split()]
+        # boxes = open("out.box").readlines()
+        # boxes = [line.strip() for line in boxes if not line.startswith("~")]
+        # results = []
+        # assert len(boxes) == len(''.join(words))
+        # for word in words:
+            # chars = boxes[:len(word)]
+            # values = [v.split(" ") for v in chars]
+            # boxes = boxes[len(word):]
+            # left = min([float(v[1]) for v in values])
+            # bottom = min([float(v[2]) for v in values])
+            # right = max([float(v[3]) for v in values])
+            # top = max([float(v[4]) for v in values])
+            # results.append([word, [left, bottom, right, top]])
         return results
